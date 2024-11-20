7f1b7917d31c5e01b657a5a7afe6f1aeff2d8d78
jpai-15 <pai.jhanvipai@gmail.com>
2024-11-19 19:34:02 -0500

    login component

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,2 +1,46 @@
 import React, { useState } from 'react';
 import axios from "axios";
+
+const Login: React.FC = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [error, setError] = useState("");
+
+    //Setting values for formData after user input 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    //Using Axios to send to backend
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post("/api/login", formData);
+            console.log(response.data);
+            setError("");
+        } catch (err) {
+            setError("Login Failed");
+        }
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={handleChange}
+            />
+            <input 
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+            />
+            {error && <p>{error}</p>}
+            <button type="submit">Login</button>
+        </form>
+    );
+};
