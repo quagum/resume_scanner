adb11e5d3ff36f8005a91d276989eed85b07c253
jpai-15 <pai.jhanvipai@gmail.com>
2024-11-21 15:22:35 -0500

    page routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './styles/App.css';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Render the Sign Up component as landing page*/}
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
