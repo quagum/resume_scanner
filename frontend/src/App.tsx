174d0a0f0af828769274a08d45bc35e567b98df3
jpai-15 <pai.jhanvipai@gmail.com>
2024-11-22 00:16:37 -0500

    adding input form routing + bug fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,23 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './styles/App.css';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import ResumeJobInputForm from "./components/InputComp";
+
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Render the Login component as landing page*/}
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/inputForm" element={<ResumeJobInputForm />} />
+      </Routes>
+    </Router>
   );
 };
 
